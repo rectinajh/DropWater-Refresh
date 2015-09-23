59f6e18629cf7f6e5686c6e945c85d5daed8ed74
H Jiang <rectinajh@163.com>
2015-09-23 08:31:34 +0800

    waterDrop refreshing.

---
+++ SlimeRefresh/SRTViewController.m
@@ -0,0 +1,109 @@
+//
+//  SRTViewController.m
+//  SlimeRefresh
+//
+//  Created by zrz on 12-6-15.
+//  Copyright (c) 2012年 zrz. All rights reserved.
+//
+
+#import "SRTViewController.h"
+#import "SRRefreshView.h"
+
+@interface SRTViewController ()
+<UITableViewDelegate, SRRefreshDelegate>
+
+@end
+
+@implementation SRTViewController {
+    SRRefreshView   *_slimeView;
+    UITableView     *_tableView;
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = @"SlimeRefresh";
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    CGRect bounds = self.view.bounds;
+    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, bounds.size.width, bounds.size.height)];
+    bounds.size.height += 1;
+    _tableView.delegate = self;
+    [self.view addSubview:_tableView];
+    
+    _slimeView = [[SRRefreshView alloc] init];
+    _slimeView.delegate = self;
+    _slimeView.slimeMissWhenGoingBack = YES;
+    _slimeView.slime.bodyColor = [UIColor redColor];
+    _slimeView.slime.skinColor = [UIColor whiteColor];
+    _slimeView.slime.lineWith = 1;
+    _slimeView.slime.shadowBlur = 4;
+    _slimeView.slime.shadowColor = [UIColor redColor];
+    
+    UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, bounds.size.width, 40)];
+    headerView.backgroundColor = [UIColor whiteColor];
+    _tableView.tableHeaderView = headerView;
+    _tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth| UIViewAutoresizingFlexibleHeight;
+    [_tableView addSubview:_slimeView];
+    _tableView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0);
+    
+    [_slimeView update:64];
+}
+
+- (void)dealloc
+{
+    [_slimeView removeFromSuperview];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    [_slimeView removeFromSuperview];
+    [_tableView removeFromSuperview];
+    _slimeView = nil;
+    _tableView = nil;
+}
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    return UIInterfaceOrientationMaskAll;
+}
+
+- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
+{
+    if (UIInterfaceOrientationIsPortrait(self.interfaceOrientation)) {
+        [_slimeView update:64];
+    }else {
+        [_slimeView update:32];
+    }
+}
+
+#pragma mark - scrollView delegate
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    [_slimeView scrollViewDidScroll];
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
+{
+    [_slimeView scrollViewDidEndDraging];
+}
+
+#pragma mark - slimeRefresh delegate
+
+- (void)slimeRefreshStartRefresh:(SRRefreshView *)refreshView
+{
+    [_slimeView performSelector:@selector(endRefresh)
+                     withObject:nil afterDelay:3
+                        inModes:[NSArray arrayWithObject:NSRunLoopCommonModes]];
+}
+
+@end
